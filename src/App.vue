f975669df8af87205f9c570d067a148d0e2c6899
victornwakpa <nwakpavictor@gmail.com>
2023-08-21 18:54:51 +0100

    added two extra card in the card section

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
 <template>
   <main class="bg-color min-h-full max-w-full">
-    <div class="md:px-12 py-8 w-full text-white min-h-screen">
+    <div class="px-20 py-8 w-full text-white min-h-screen">
       <!-- NAVIGATION BAR ======================= -->
       <div class="flex justify-between items-center">
         <div>
@@ -36,19 +36,56 @@
 
       <!-- CARD SECTION ==================== -->
       <div class="h-screen-calc">
-        <div class="card-color p-8 rounded-3xl max-w-sm">
-          <img class="mx-auto" src="./assets/images/1.svg" alt="">
-          <div class="text-center flex flex-col items-center">
-            <div class="mt-5 text-2xl font-extrabold ">Trade Desk</div>
-            <p class="mt-5">Invest in crypto anytime, anywhere with our safe, secure, and easy to use online platform</p>
-            <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
-              <span class="block">Get Started</span>
-              <div class="block">
-                <svg class="fill-white h-5 w-5">
-                  <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
-                </svg>
-              </div>
-            </button>
+        <div class="grid grid-cols-3 gap-5">
+          <!-- FIRST CARD -->
+          <div class="card-color p-6 rounded-3xl max-w-sm">
+            <img class="mx-auto" src="./assets/images/1.svg" alt="">
+            <div class="text-center flex flex-col items-center">
+              <div class="mt-5 text-3xl font-extrabold ">Trade Desk</div>
+              <p class="mt-5 text-gray-400 text-sm">Invest in crypto anytime, anywhere with our safe, secure, and easy to use online platform</p>
+              <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
+                <span class="block">Get Started</span>
+                <div class="block">
+                  <svg class="fill-white h-5 w-5">
+                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                </div>
+              </button>
+            </div>
+          </div>
+
+          <!-- SECOND CARD -->
+          <div class="card-color p-6 rounded-3xl max-w-md">
+            <img class="mx-auto" src="./assets/images/2.svg" alt="">
+            <div class="text-center flex flex-col items-center">
+              <div class="mt-5 text-3xl font-extrabold ">CoinFlip ATMs</div>
+              <p class="mt-5 text-gray-400 text-sm">We have thousands of ATMs located across the U.S. where you can easily convert cash to crypto</p>
+              <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
+                <span class="block">Find an ATM</span>
+                <div class="block">
+                  <svg class="fill-white h-5 w-5">
+                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                </div>
+              </button>
+            </div>
+          </div>
+
+          <!-- THIRD CARD -->
+          <div class="card-color p-6 rounded-3xl max-w-md">
+            <img class="mx-auto" src="./assets/images/3.svg" alt="">
+            <div class="text-center flex flex-col items-center">
+              <div class="mt-5 text-3xl font-extrabold ">CoinFlip Wallet</div>
+              <p class="mt-5 text-gray-400 text-sm">IStore your growing investments in our non-custodial wallet that gives you access to a full suite of DeFi services in one place</p>
+              <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
+                <span class="block">Download the App</span>
+                <div class="block">
+                  <svg class="fill-white h-5 w-5">
+                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                </div>
+              </button>
+            </div>
           </div>
         </div>
       </div>
