9f6fb6ba551b8edfe37e9381aafd4e38f6e5366e
victornwakpa <nwakpavictor@gmail.com>
2023-08-22 16:41:30 +0100

    fixed a css issue

--- src/App.vue
+++ src/App.vue
@@ -91,7 +91,7 @@
       </div>
 
       <!-- CLICK CATCH ============================= -->
-      <div class="h-screen-calc mb-24">
+      <div class="">
         <!-- FIRST SECTION ========================= -->
         <div class="text-center mx-auto max-w-3xl mb-48 mt-44 ">
           <h3 class="font-extrabold text-5xl py-3">
@@ -106,7 +106,7 @@
         </div>
 
         <!-- SECOND SECTION ========================= -->
-        <div class="flex items-center justify-ceter">
+        <div class="flex items-center justify-center">
           <div class="flex-1 grid place-content-center">
             <img class="w-52 text-center" src="./assets/images/icons.svg" alt="">
           </div>
@@ -120,6 +120,25 @@
             <button class="m-2 rounded-md p-2 border-2 border-white">Get Started</button>
           </div>
         </div>
+
+      </div>
+
+      <!--  BUY AND SELL SECTION ================= -->
+      <div>
+        <h3>
+          Buy and sell with the lowest fees in the industry
+        </h3>
+        <p>
+          Buy and sell 150+ cryptocurrencies with 20+ fiat currencies using bank transfers or your credit/debit card.
+        </p>
+        <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
+          <span class="block">Get Started</span>
+          <div class="block">
+            <svg class="fill-white h-5 w-5">
+              <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+            </svg>
+          </div>
+        </button>
       </div>
 
     </div>
