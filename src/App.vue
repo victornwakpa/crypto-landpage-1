1df3668b073fdad4c1541598af3e3817c2694197
victornwakpa <nwakpavictor@gmail.com>
2023-08-23 15:43:13 +0100

    Added Table component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
   import Card from "./components/Card.vue";
+  import Table from "./components/Table.vue"
 </script>
 
 <template>
@@ -35,29 +36,7 @@
         <button class="mt-5 px-5 py-3 bg-purple-700 rounded-xl font-semibold">Get Started</button>
       </div>
 
-      <!-- CARD SECTION ==================== -->
-      <div class="h-screen-calc grid place-content-center grid-cols-3 gap-5">
-        <div v-for="card in cards" class="grid">
-          <!-- FIRST CARD -->
-          <div class="card-color p-6 rounded-3xl max-w-sm">
-            <img class="mx-auto" :src="card.img" alt="">
-            <div class="text-center flex flex-col items-center">
-              <div class="mt-5 text-3xl font-extrabold ">{{card.title}}</div>
-              <p class="mt-5 text-gray-400 text-lg">{{ card.desc }}</p>
-              <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
-                <span class="block">{{ card.button }}</span>
-                <div class="block">
-                  <svg class="fill-white h-5 w-5">
-                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
-                  </svg>
-                </div>
-              </button>
-            </div>
-          </div>
-
-      
-        </div>
-      </div>
+      <Card />
 
       <!-- CLICK CATCH ============================= -->
       <div class="">
@@ -105,111 +84,9 @@
           </div>
       </div>
 
-      <div>
-        <table class="">
-          <tbody class="border-custom">
-            <tr class="">
-              <th>Bitcoin</th>
-              <td class="p-3 text-sm text-gray-700">BTC</td>
-              <td class="p-3 text-sm text-gray-700">$56,290.30</td>
-              <td class="p-3 text-sm text-gray-700">+1.68%</td>
-              <td class="p-3 text-sm text-gray-700">
-                <img src="./assets/images/graph-1.svg" alt="">
-              </td>
-              <td class="p-3 text-sm text-gray-700">
-                <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
-                <span class="block">Get Started</span>
-                <div class="block">
-                  <svg class="fill-white h-5 w-5">
-                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
-                  </svg>
-                </div>
-              </button>
-              </td>
-            </tr>
+      <Table />
 
-            <tr>
-              <th>Ethereum</th>
-              <td class="p-3 text-sm text-gray-700">ETH</td>
-              <td class="p-3 text-sm text-gray-700">$56,290.30</td>
-              <td class="p-3 text-sm text-gray-700">+1.68%</td>
-              <td class="p-3 text-sm text-gray-700">
-                <img src="./assets/images/graph-2.svg" alt="">
-              </td>
-              <td class="p-3 text-sm text-gray-700">
-                <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
-                <span class="block">Get Started</span>
-                <div class="block">
-                  <svg class="fill-white h-5 w-5">
-                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
-                  </svg>
-                </div>
-              </button>
-              </td>
-            </tr>
-
-            <tr>
-              <th>Cardano</th>
-              <td class="p-3 text-sm text-gray-700">ADA</td>
-              <td class="p-3 text-sm text-gray-700">$56,290.30</td>
-              <td class="p-3 text-sm text-gray-700">+1.68%</td>
-              <td class="p-3 text-sm text-gray-700">
-                <img src="./assets/images/graph-3.svg" alt="">
-              </td>
-              <td class="p-3 text-sm text-gray-700">
-                <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
-                <span class="block">Get Started</span>
-                <div class="block">
-                  <svg class="fill-white h-5 w-5">
-                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
-                  </svg>
-                </div>
-              </button>
-              </td>
-            </tr>
-
-            <tr>
-              <th>Wax</th>
-              <td class="p-3 text-sm text-gray-700">WAXP</td>
-              <td class="p-3 text-sm text-gray-700">$56,290.30</td>
-              <td class="p-3 text-sm text-gray-700">+1.68%</td>
-              <td class="p-3 text-sm text-gray-700">
-                <img src="./assets/images/graph-4.svg" alt="">
-              </td>
-              <td class="p-3 text-sm text-gray-700">
-                <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
-                <span class="block">Get Started</span>
-                <div class="block">
-                  <svg class="fill-white h-5 w-5">
-                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
-                  </svg>
-                </div>
-              </button>
-              </td>
-            </tr>
-
-            <tr>
-              <th>Polkadot</th>
-              <td class="p-3 text-sm text-gray-700">DOT</td>
-              <td class="p-3 text-sm text-gray-700">$56,290.30</td>
-              <td class="p-3 text-sm text-gray-700">+1.68%</td>
-              <td class="p-3 text-sm text-gray-700">
-                <img src="./assets/images/graph-5.svg" alt="">
-              </td>
-              <td>
-                <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
-                <span class="block">Get Started</span>
-                <div class="block">
-                  <svg class="fill-white h-5 w-5">
-                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
-                  </svg>
-                </div>
-              </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      
 
     </div>
     
---
+++ src/components/Table.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="grid place-content-center">
+        <table class="border-2 border-collapse rounded-xl overflow-hidden bg-slate-600">
+          <tbody class="border-custom">
+            <tr class="">
+              <th>Bitcoin</th>
+              <td class="p-3 text-sm text-gray-700">BTC</td>
+              <td class="p-3 text-sm text-gray-700">$56,290.30</td>
+              <td class="p-3 text-sm text-gray-700">+1.68%</td>
+              <td class="p-3 text-sm text-gray-700">
+                <img src="./assets/images/graph-1.svg" alt="">
+              </td>
+              <td class="p-3 text-sm text-gray-700">
+                <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
+                <span class="block">Get Started</span>
+                <div class="block">
+                  <svg class="fill-white h-5 w-5">
+                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                </div>
+              </button>
+              </td>
+            </tr>
+
+            <tr>
+              <th>Ethereum</th>
+              <td class="p-3 text-sm text-gray-700">ETH</td>
+              <td class="p-3 text-sm text-gray-700">$56,290.30</td>
+              <td class="p-3 text-sm text-gray-700">+1.68%</td>
+              <td class="p-3 text-sm text-gray-700">
+                <img src="./assets/images/graph-2.svg" alt="">
+              </td>
+              <td class="p-3 text-sm text-gray-700">
+                <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
+                <span class="block">Get Started</span>
+                <div class="block">
+                  <svg class="fill-white h-5 w-5">
+                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                </div>
+              </button>
+              </td>
+            </tr>
+
+            <tr>
+              <th>Cardano</th>
+              <td class="p-3 text-sm text-gray-700">ADA</td>
+              <td class="p-3 text-sm text-gray-700">$56,290.30</td>
+              <td class="p-3 text-sm text-gray-700">+1.68%</td>
+              <td class="p-3 text-sm text-gray-700">
+                <img src="./assets/images/graph-3.svg" alt="">
+              </td>
+              <td class="p-3 text-sm text-gray-700">
+                <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
+                <span class="block">Get Started</span>
+                <div class="block">
+                  <svg class="fill-white h-5 w-5">
+                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                </div>
+              </button>
+              </td>
+            </tr>
+
+            <tr>
+              <th>Wax</th>
+              <td class="p-3 text-sm text-gray-700">WAXP</td>
+              <td class="p-3 text-sm text-gray-700">$56,290.30</td>
+              <td class="p-3 text-sm text-gray-700">+1.68%</td>
+              <td class="p-3 text-sm text-gray-700">
+                <img src="./assets/images/graph-4.svg" alt="">
+              </td>
+              <td class="p-3 text-sm text-gray-700">
+                <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
+                <span class="block">Get Started</span>
+                <div class="block">
+                  <svg class="fill-white h-5 w-5">
+                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                </div>
+              </button>
+              </td>
+            </tr>
+
+            <tr>
+              <th>Polkadot</th>
+              <td class="p-3 text-sm text-gray-700">DOT</td>
+              <td class="p-3 text-sm text-gray-700">$56,290.30</td>
+              <td class="p-3 text-sm text-gray-700">+1.68%</td>
+              <td class="p-3 text-sm text-gray-700">
+                <img src="./assets/images/graph-5.svg" alt="">
+              </td>
+              <td>
+                <button class=" mt-5 flex bg-slate-600 px-5 py-3 rounded-lg">
+                <span class="block">Get Started</span>
+                <div class="block">
+                  <svg class="fill-white h-5 w-5">
+                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+                  </svg>
+                </div>
+              </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+</template>
