2d234dcefaf906a81ace6a69995d6d61a48a50e4
victornwakpa <nwakpavictor@gmail.com>
2023-08-24 14:57:58 +0100

    added amazing styling to the footer section

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,34 +1,38 @@
 <template>
-    <footer>
-        <div class="flex justify-between ">
-            <div class="flex-1">
-                <h4>CoinFlip</h4>
-                <p>CoinFlip, the world's leading bitcoin ATM operator, makes it so flippin' easy to buy and sell bitcoin via cash, card, or bank transfer.</p>
-                <p>Sign up to get the latest in CoinFlip news, discounts, and more.</p>
+    <footer class="grid place-content-center p-12 border-t-2 border-yellow-50">
+        <div class="flex justify-between text-gray-400">
+            <div class="flex-1 p-2">
+                <h4 class="uppercase tracking-widest text-3xl my-4">CoinFlip</h4>
+                <p class="my-3 text-sm">CoinFlip, the world's leading bitcoin ATM operator, makes it so flippin' easy to buy and sell bitcoin via cash, card, or bank transfer.</p>
+                <p class="my-3 text-sm">Sign up to get the latest in CoinFlip news, discounts, and more.</p>
 
                 <form action="">
-                    <input type="text" placeholder="Email Address">
+                    <input type="text" class="p-2 rounded-lg bg-transparent border-2 my-3" placeholder="Email Address">
                 </form>
 
-                <p>2021 GPD Holdings, LLC FinCEN MSB</p>
+                <p class="my-3 text-sm">2021 GPD Holdings, LLC FinCEN MSB</p>
             </div>
 
-            <div class="flex-1 flex flex-col">
-                <a href="#" class="block">Company</a>
-                <a href="#" class="block">About</a>
-                <a href="#" class="block">Careers</a>
-                <a href="#" class="block">Press</a>
-                <a href="#" class="block">News</a>
-                <a href="#" class="block">Merch</a>
+            <div class="flex-1 flex items-center flex-col">
+                <div class="my-4 p-2 flex flex-col gap-y-4">
+                    <a href="#" class="block">Company</a>
+                    <a href="#" class="block">About</a>
+                    <a href="#" class="block">Careers</a>
+                    <a href="#" class="block">Press</a>
+                    <a href="#" class="block">News</a>
+                    <a href="#" class="block">Merch</a>
+                </div>
             </div>
 
-            <div class="flex-1" grid place-content-center>
-                <a href="#" class="block">Privacy Policy and Terms of Service</a>
-                <a href="#" class="block">Coinflip Privacy Policy</a>
-                <a href="#" class="block">Coinflip Biometric Privacy Policy</a>
-                <a href="#" class="block">CoinFlip Financial Privacy Notice</a>
-                <a href="#" class="block">CoinFlip Terms of Service</a>
-                <a href="#" class="block">Coinflip Trade Desk Terms of Service</a>
+            <div class="flex-1">
+                <div class="my-4 p-2 flex flex-col gap-y-4">
+                    <a href="#" class="block">Privacy Policy and Terms of Service</a>
+                    <a href="#" class="block">Coinflip Privacy Policy</a>
+                    <a href="#" class="block">Coinflip Biometric Privacy Policy</a>
+                    <a href="#" class="block">CoinFlip Financial Privacy Notice</a>
+                    <a href="#" class="block">CoinFlip Terms of Service</a>
+                    <a href="#" class="block">Coinflip Trade Desk Terms of Service</a>
+                </div>
             </div>
         </div>
     
